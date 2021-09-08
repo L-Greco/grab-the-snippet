31e1a5b6b9e39a31c22e597db5775eeecc702e7f
L-Greco <makaronaskonstandinos@gmail.com>
2021-09-08 11:36:09 +0300

    fixed the scroll bar in y axis

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import './styles/myBootstrap.css';
+// import "./styles/bootstrap.css"
 import App from './App';
 import { Provider } from "react-redux"
 import configureStore from "./redux/store"
