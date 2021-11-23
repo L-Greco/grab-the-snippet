1e8552a2aef6401ceac0ddff97f305e67659ed14
L-Greco <makaronaskonstandinos@gmail.com>
2021-11-23 20:11:59 +0200

    some css changes

--- src/components/StartingPage.jsx
+++ src/components/StartingPage.jsx
@@ -39,29 +39,74 @@ function StartingPage() {
           </div>
         </div>
 
-        <div style={{ fontFamily: "Acme", display: "flex" }}>
-          <Link to="/loginPage">Log In</Link>
+        <div
+          style={{
+            fontFamily: "Acme",
+            display: "flex",
+          }}
+        >
+          <Link style={{ color: "white" }} to="/loginPage">
+            Log In
+          </Link>
         </div>
       </header>
 
-      <section id="opening" className="sp-section1 d-flex">
-        <div class="jumbotron mx-auto">
+      <section id="opening" className="sp-section1 d-flex ">
+        <div className="jumbotron mx-auto">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-7">
+                <h1 className="mt-0">
+                  Grab The Snippet helps developers be faster than ever.
+                </h1>
+                <p className="lead">
+                  Store your code snippets as cards and have access on them with
+                  a click!
+                </p>
+                <hr className="my-4" />
+                <p>
+                  You can either open the snippet and see the code or copy to
+                  clipboard with a click.
+                </p>
+                <Link to="/loginPage">
+                  <button
+                    className="btn btn-primary "
+                    style={{ fontFamily: "Acme" }}
+                  >
+                    Login-it's free
+                  </button>
+                </Link>
+              </div>
+              <div className="d-none d-md-block col-5">
+                <img
+                  className="img-fluid"
+                  src="pic2.png"
+                  alt=""
+                  style={{ height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="opening" className="sp-section2 d-flex">
+        <div className="jumbotron mx-auto">
           <div className="container">
             <div className="row">
               <div className="col-12 col-md-8">
                 <h1>Grab The Snippet helps developers be faster than ever.</h1>
-                <p class="lead">
+                <p className="lead">
                   Store your code snippets as cards and have access on them with
                   a click!
                 </p>
-                <hr class="my-4" />
+                <hr className="my-4" />
                 <p>
                   You can either open the snippet and see the code or copy the
                   code to clipboard with a click.
                 </p>
                 <Link to="/loginPage">
                   <button
-                    class="btn btn-primary "
+                    className="btn btn-primary "
                     style={{ fontFamily: "Acme" }}
                   >
                     Login-it's free
@@ -80,15 +125,82 @@ function StartingPage() {
           </div>
         </div>
       </section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <div style={{ height: "500px", color: "blue" }}>sss</div>
-      <button onClick={() => console.log(scroll)}></button>
-      <div style={{ height: "500px", color: "blue" }}>sss</div>
-      <button onClick={() => console.log(scroll)}></button>
-      <div style={{ height: "500px", color: "blue" }}>sss</div>
-      <button onClick={() => console.log(scroll)}></button>
+      <section id="opening" className="sp-section1 d-flex">
+        <div className="jumbotron mx-auto">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-8">
+                <h1>Grab The Snippet helps developers be faster than ever.</h1>
+                <p className="lead">
+                  Store your code snippets as cards and have access on them with
+                  a click!
+                </p>
+                <hr className="my-4" />
+                <p>
+                  You can either open the snippet and see the code or copy the
+                  code to clipboard with a click.
+                </p>
+                <Link to="/loginPage">
+                  <button
+                    className="btn btn-primary "
+                    style={{ fontFamily: "Acme" }}
+                  >
+                    Login-it's free
+                  </button>
+                </Link>
+              </div>
+              <div className="d-none d-md-block col-4">
+                <img
+                  className="img-fluid"
+                  src="pic2.png"
+                  alt=""
+                  style={{ height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        style={{ color: "white" }}
+        id="opening"
+        className="sp-section1 d-flex"
+      >
+        <div className="jumbotron mx-auto">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-8">
+                <h1>Grab The Snippet helps developers be faster than ever.</h1>
+                <p className="lead">
+                  Store your code snippets as cards and have access on them with
+                  a click!
+                </p>
+                <hr className="my-4" />
+                <p>
+                  You can either open the snippet and see the code or copy the
+                  code to clipboard with a click.
+                </p>
+                <Link to="/loginPage">
+                  <button
+                    className="btn btn-primary "
+                    style={{ fontFamily: "Acme" }}
+                  >
+                    Login-it's free
+                  </button>
+                </Link>
+              </div>
+              <div className="d-none d-md-block col-4">
+                <img
+                  className="img-fluid"
+                  src="pic2.png"
+                  alt=""
+                  style={{ height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
